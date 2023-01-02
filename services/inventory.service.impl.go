3af311fb433a290da4cb6a1b620db44d4e45b159
Binod Thapa <binod.thapa@non.keysight.com>
2023-01-02 18:40:17 +0530

    add product and update product

--- services/inventory.service.impl.go
+++ services/inventory.service.impl.go
@@ -7,18 +7,19 @@ import (
 
 	"github.com/binod210/go-inventory-management/authentication"
 	"github.com/binod210/go-inventory-management/db"
+	"github.com/binod210/go-inventory-management/models"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type InventoryServiceImpl struct {
-	Repository *db.InventoryRepository
+	Repository db.InventoryRepository
 	Auth       *authentication.JWT
 }
 
 func NewInventoryService(Db *mongo.Database, auth *authentication.JWT) InventoryService {
 	repo := db.NewInventoryRepository(Db)
 	return &InventoryServiceImpl{
-		Repository: &repo,
+		Repository: repo,
 		Auth:       auth,
 	}
 }
@@ -33,6 +34,20 @@ func (is *InventoryServiceImpl) AddProduct(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	var product models.ProductDecode
+	err = json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	savedProduct, err := is.Repository.Save(&product)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	json.NewEncoder(w).Encode(savedProduct)
+
 }
 
 func (is *InventoryServiceImpl) UpdateProduct(w http.ResponseWriter, r *http.Request) {
@@ -45,4 +60,38 @@ func (is *InventoryServiceImpl) UpdateProduct(w http.ResponseWriter, r *http.Req
 		json.NewEncoder(w).Encode(err)
 		return
 	}
+
+	var product models.ProductDecode
+	err = json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	if product.Id == "" {
+		json.NewEncoder(w).Encode("Id cannot be null")
+		return
+	}
+
+	dbProduct, err := is.Repository.FindById(product.Id)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	if product.Name == "" {
+		dbProduct.Name = product.Name
+	}
+	if product.Qty == 0 {
+		dbProduct.Qty = product.Qty
+	}
+	if product.Rate == 0 {
+		dbProduct.Rate = product.Rate
+	}
+	dbProduct, err = is.Repository.Save(dbProduct)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	json.NewEncoder(w).Encode(dbProduct)
 }
